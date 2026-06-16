831853c593782f84bf1e1c2601c88dca6d3b4695
Alexey Vanyukevich <alexey.vanyukevich@gmail.com>
2026-06-16 16:20:32 +0200

    Init logging project

---
+++ UBP.Logging/Bootstrap.cs
@@ -0,0 +1,35 @@
+using System.Globalization;
+
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace UBP.Logging;
+
+public static class Bootstrap
+{
+    public static IHostBuilder AddLogging(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.UseSerilog((context, configuration) =>
+        {
+            configuration
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+                .WriteTo.File(
+                    formatter: new CompactJsonFormatter(),
+                    path: "/logs/log-.txt",
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true)
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithThreadId()
+                .ReadFrom.Configuration(context.Configuration);
+        });
+
+        return hostBuilder;
+    }
+}
