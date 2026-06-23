e74a920de277920d295ddc924083e6c24e0e992b
Alexey Vanyukevich <alexey.vanyukevich@gmail.com>
2026-06-23 23:24:40 +0200

    Added Options from configuration project

---
+++ UBP.Options.Configuration/BindConfigureOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace UBP.Options.Configuration;
+
+public class BindConfigureOptions<TOptions>(IConfiguration configuration) : IConfigureOptions<TOptions> where TOptions : class
+{
+    protected IConfiguration Configuration => configuration;
+    public virtual void Configure(TOptions options)
+    {
+        Configuration.Bind(options);
+    }
+}
