66890f42b3a3bf4ec6fd99c1615925b014fd0e4d
Alexey Vanyukevich <alexey.vanyukevich@gmail.com>
2026-06-23 23:20:25 +0200

    Added DbOptions class

---
+++ UBP.Core/UBP.Core.Persistence/Options/DbOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UBP.Core.Persistence.Options;
+
+public class DbOptions
+{
+    public string ConnectionString { get; set; }
+}
