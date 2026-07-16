f64711dc6b1ce71191cca34a8472d286ac15bb49
AlexeyVanyukevich <alexey.vanyukevich@gmail.com>
2026-07-17 01:17:43 +0300

    Updated booking persistence layer to use an extension method from Core to connect to PostgreSQL

--- UBP.Booking/UBP.Booking.Persistence/Bootstrap.cs
+++ UBP.Booking/UBP.Booking.Persistence/Bootstrap.cs
@@ -1,20 +1,21 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 using UBP.Booking.Persistence.Contexts;
 using UBP.Booking.Persistence.Interfaces;
 using UBP.Booking.Persistence.Repositories;
 using UBP.Core.Data.EF;
+using UBP.Core.Persistence.Database.Options;
+using UBP.Core.Persistence.EF.PostgreSQL;
 
 namespace UBP.Booking.Persistence;
 
 public static class Bootstrap
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder> optionsAction)
+    public static IServiceCollection AddPersistence(this IServiceCollection services, Action<DbOptions>? configure = null)
     {
         services.AddInterceptors();
 
-        services.AddDbContextPool<AppDbContext>(optionsAction);
+        services.AddPostgresEFDbContext<AppDbContext, DbOptions>(configure);
         services.AddUnitOfWork(sp => sp.GetRequiredService<AppDbContext>());
         services.AddScoped<IAvailabilitySlotRepository, AvailabilitySlotRepository>();
 
