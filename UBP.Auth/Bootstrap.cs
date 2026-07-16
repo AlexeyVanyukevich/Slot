3138731227f9dc8bdba6889302e1ab285f429a7a
AlexeyVanyukevich <alexey.vanyukevich@gmail.com>
2026-07-17 01:22:24 +0300

    OpenID Connect auth extension

--- UBP.Auth/Bootstrap.cs
+++ UBP.Auth/Bootstrap.cs
@@ -1,20 +1,27 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using OpenIddict.Validation;
 
 namespace UBP.Auth;
 
 public static class Bootstrap
 {
-    public static IServiceCollection AddIamAuthentication(this IServiceCollection services, Action<IamAuthenticationOptions> configure)
+    public static IServiceCollection AddIamAuthentication(this IServiceCollection services, Action<IamAuthenticationOptions>? configure = null)
     {
-        var options = new IamAuthenticationOptions();
-        configure(options);
+        if (configure is not null)
+            services.Configure(configure);
+
+        services.AddOptions<OpenIddictValidationOptions>()
+            .Configure<IOptions<IamAuthenticationOptions>>((validationOptions, iamOptions) =>
+            {
+                validationOptions.Issuer = new Uri(iamOptions.Value.Authority);
+                validationOptions.Audiences.UnionWith([iamOptions.Value.Audience]);
+            });
 
         services.AddOpenIddict()
             .AddValidation(validationOptions =>
             {
-                validationOptions.SetIssuer(options.Authority);
-                validationOptions.AddAudiences(options.Audience);
-
                 validationOptions.UseSystemNetHttp();
                 validationOptions.UseAspNetCore();
             });
